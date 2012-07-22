8736608abc24027caa4a00d08633ea10be6c8c34
Steven Robertson <steven@strobe.cc>
2012-07-22 15:54:42 -0700

    Internal network change for helper script

--- dist/addrs.py
+++ dist/addrs.py
@@ -1,4 +1,4 @@
-ip = '10.1.10.2'
+ip = '10.1.10.4'
 port = 12615
 names = 'tasks tasks_loprio workers responses'.split()
 addrs = dict((k, 'tcp://%s:%d' % (ip, port+i)) for i, k in enumerate(names))
