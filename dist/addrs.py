328682f936980b21a6a10b94927d740426a339f7
Steven Robertson <steven@strobe.cc>
2013-12-21 15:32:21 -0800

    Localhost by default

--- dist/addrs.py
+++ dist/addrs.py
@@ -1,4 +1,4 @@
-ip = '10.1.10.4'
+ip = '127.0.0.1'
 port = 12615
 names = 'tasks tasks_loprio workers responses'.split()
 addrs = dict((k, 'tcp://%s:%d' % (ip, port+i)) for i, k in enumerate(names))
